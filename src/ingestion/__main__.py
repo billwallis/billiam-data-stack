2e2bbedc9106ed8f7379c95ededfd5797f5d79ec
Bill <bill@thewallisfamily.co.uk>
2026-03-29 17:12:05 +0100

    feat: tweak MotherDuck configuration

--- src/ingestion/__main__.py
+++ src/ingestion/__main__.py
@@ -3,9 +3,21 @@ import os
 import src.ingestion.github
 
 
+def _ensure_env(env_var: str) -> str:
+    try:
+        return os.environ[env_var]
+    except KeyError as e:
+        err_msg = f"Missing required environment variable: {env_var}"
+        raise OSError(err_msg) from e
+
+
 def main() -> int:
+    _ensure_env("MOTHERDUCK_TOKEN")
+    github_token = _ensure_env("GITHUB_API_TOKEN")
+
+    # Run GitHub
     src.ingestion.github.github_pipeline(
-        access_token=os.environ["GITHUB_API_TOKEN"],
+        access_token=github_token,
         username="billwallis",
     )
 
