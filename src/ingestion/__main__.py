d70f594afb4720d97d2169f1e2f9842a1e4abceb
Bill <bill@thewallisfamily.co.uk>
2026-03-29 18:48:32 +0100

    feat: add Google Sheets pipeline

--- src/ingestion/__main__.py
+++ src/ingestion/__main__.py
@@ -1,6 +1,7 @@
 import os
 
 import src.ingestion.github
+import src.ingestion.google_sheets
 
 
 def _ensure_env(env_var: str) -> str:
@@ -13,12 +14,28 @@ def _ensure_env(env_var: str) -> str:
 
 def main() -> int:
     _ensure_env("MOTHERDUCK_TOKEN")
-    github_token = _ensure_env("GITHUB_API_TOKEN")
 
     # Run GitHub
+    github_token = _ensure_env("GITHUB_API_TOKEN")
     src.ingestion.github.github_pipeline(
         access_token=github_token,
         username="billwallis",
+    )
+
+    # Run Google Sheets
+    monzo_id = _ensure_env("GOOGLE_SHEETS__MONZO_TRANSACTIONS_ID")
+    monzo_name = _ensure_env("GOOGLE_SHEETS__MONZO_TRANSACTIONS_NAME")
+    finances_id = _ensure_env("GOOGLE_SHEETS__FINANCES_ID")
+    finances_name = _ensure_env("GOOGLE_SHEETS__FINANCES_NAME")
+    # finances_history_id = _ensure_env("GOOGLE_SHEETS__FINANCES_HISTORY_ID")
+    # finances_history_name = _ensure_env("GOOGLE_SHEETS__FINANCES_HISTORY_NAME")
+    src.ingestion.google_sheets.google_sheets_pipeline(
+        google_service=src.ingestion.google_sheets.build_google_client_service(),
+        google_sheets=[
+            ("monzo_transactions", monzo_id, monzo_name),
+            ("finances", finances_id, finances_name),
+            # ("finances_history", finances_history_id, finances_history_name),
+        ],
     )
 
     return 0
