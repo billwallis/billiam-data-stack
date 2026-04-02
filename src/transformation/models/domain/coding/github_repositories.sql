1ab5a6e09cb03ad8d417a2672d1149d4492ed25c
Bill Wallis <bill@tasman.ai>
2026-04-02 17:38:33 +0100

    feat: list issues by url in GitHub setting audit

--- src/transformation/models/domain/coding/github_repositories.sql
+++ src/transformation/models/domain/coding/github_repositories.sql
@@ -149,33 +149,49 @@ audit (
     name assert__repository_settings_are_correct,
     blocking false,
 );
-from warehouse.coding.github_repositories
-where 1=1
-    and username = owner_username  /* my own repos */
-    and not is_archived
-    and (0=1
-        or nullif(description, '') is null
-        or not ends_with(description, '.')
-        or (not is_private and not auto_merge_allowed)
-        or not delete_branch_on_merge
-        or not forking_allowed
-        or has_discussions_enabled
-        or not has_issues_enabled
-        or has_projects_enabled
-        or not has_vulnerability_alerts_enabled
-        or has_wiki_enabled
-        or not rebase_merge_allowed
-        or not merge_commit_allowed
-        -- or merge_commit_title != ''
-        -- or merge_commit_message != ''
-        or not squash_merge_allowed
-        -- or squash_merge_title != ''
-        -- or squash_merge_message != ''
-        or not plan_features__codeowners
-        or not plan_features__draft_pull_requests
-        or web_commit_signoff_required
-        or is_disabled
-        or is_empty
-        or is_locked
-    )
+with my_active_repos as (
+    select
+        url,
+        nullif(description, '') is null as description_is_null,
+        not ends_with(description, '.') as incorrect_description_terminator,
+        (not is_private and not auto_merge_allowed) as auto_merge_not_allowed,
+        not delete_branch_on_merge as delete_branch_on_merge_not_allowed,
+        not forking_allowed as forking_not_allowed,
+        has_discussions_enabled,
+        not has_issues_enabled as issues_not_enabled,
+        has_projects_enabled,
+        not has_vulnerability_alerts_enabled as vulnerability_alerts_not_enabled,
+        has_wiki_enabled,
+        not rebase_merge_allowed as rebase_merge_not_allowed,
+        not merge_commit_allowed as merge_commit_not_allowed,
+        -- merge_commit_title != '...' as incorrect_merge_commit_title,
+        -- merge_commit_message != '...' as incorrect_merge_commit_message,
+        not squash_merge_allowed as squash_merge_not_allowed,
+        -- squash_merge_title != '...' as incorrect_squash_merge_title,
+        -- squash_merge_message != '...' as incorrect_squash_merge_message,
+        (not is_private and not plan_features__codeowners) as codeowners_not_enabled,
+        not plan_features__draft_pull_requests as draft_pull_requests_not_enabled,
+        web_commit_signoff_required,
+        is_disabled,
+        is_empty,
+        is_locked,
+    from warehouse.coding.github_repositories
+    where 1=1
+        and username = owner_username  /* my own repos */
+        and not is_archived
+)
+
+from (
+    unpivot my_active_repos
+    on columns(* exclude (url))
+    into
+        name setting
+        value is_misconfigured
+)
+select
+    url,
+    list(setting order by setting) filter (where is_misconfigured) as issues,
+group by url
+having issues is not null
+order by url
 ;
