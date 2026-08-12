afa9f157c9df107f98024b51b3772f915a42677a
Bill <Bill@TheWallisFamily.co.uk>
2026-08-12 10:01:49 +0100

    feat: add time spent per employer charts (#54)

---
+++ src/transformation/models/domain/employment/work_hours.sql
@@ -0,0 +1,47 @@
+model (
+    name warehouse.career.work_hours,
+    kind full,
+    grain (company, from_date),
+    tags (daily-tracker),
+    columns (
+        company varchar,
+        from_date date,
+        to_date date,
+        sunday decimal(4, 2),
+        monday decimal(4, 2),
+        tuesday decimal(4, 2),
+        wednesday decimal(4, 2),
+        thursday decimal(4, 2),
+        friday decimal(4, 2),
+        saturday decimal(4, 2),
+    ),
+    audits (
+        not_null(columns=[
+            company,
+            from_date,
+            to_date,
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+        ]),
+    ),
+);
+
+select
+    company,
+    from_date,
+    to_date,
+
+    sunday,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+from warehouse.raw_ods.work_hours
+;
